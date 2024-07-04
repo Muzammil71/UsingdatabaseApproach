41fa41e59c985498ea7922ea375cb1b15f4228e0
Muzammil71 <zamiikhan713@gmail.com>
2024-07-04 17:51:45 +0500

    UsingdatabaseApproach

---
+++ UsingdatabaseApproach/Data/UsingdatabaseApproachContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UsingdatabaseApproach.Models;
+
+namespace UsingdatabaseApproach.Data
+{
+    public class UsingdatabaseApproachContext : DbContext
+    {
+        public UsingdatabaseApproachContext (DbContextOptions<UsingdatabaseApproachContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UsingdatabaseApproach.Models.Medicine> Medicine { get; set; } = default!;
+    }
+}
---
+++ UsingdatabaseApproach/Models/Medicine.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsingdatabaseApproach.Models
+{
+    public class Medicine
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Weight { get; set; }
+        public string Color { get; set; }
+    }
+}
